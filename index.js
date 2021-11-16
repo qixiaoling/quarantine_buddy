d6c71b1fb24b554dbffeef4e5ed2133e6c6be5e4
qixiaoling <qixiaolinglinda@hotmail.com>
2021-11-16 12:51:39 +0100

    section "our-value" commit, javaScript

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+//Navbar: by clicking the hamburger menu, one will add the classList to the body.
+//when body has the "nav-open" class, the hidden nav will translate back to 0 and show itself.
+//by clicking any of the link, the classList on the body will be removed.
 const nav_toggle = document.querySelector(".nav-toggle");
 const nav_links = document.querySelectorAll(".nav-link");
 
@@ -10,3 +14,25 @@ nav_links.forEach(function(link){
         document.body.classList.remove("nav-open");
     })
 })
+//Tab: first remove all the active classList, then add based on the dataset.id
+const value = document.querySelector(".our-value");
+const valueBtns = document.querySelectorAll(".value-btn");
+const contents = document.querySelectorAll(".value-content");
+value.addEventListener("click", function(e){
+    const id = e.target.dataset.id;
+    if(id) {
+        valueBtns.forEach(function(btn){
+            btn.classList.remove("active");
+        })
+        e.target.classList.add("active");
+
+
+        contents.forEach(function(content){
+            content.classList.remove("active");
+        })
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+})
+
+
