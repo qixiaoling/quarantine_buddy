b0b284d4c322a53f5932f4051468fdf17e76661e
qixiaoling <qixiaolinglinda@hotmail.com>
2021-11-17 11:16:02 +0100

    section "review" commit Javascript

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-
 //Navbar: by clicking the hamburger menu, one will add the classList to the body.
 //when body has the "nav-open" class, the hidden nav will translate back to 0 and show itself.
 //by clicking any of the link, the classList on the body will be removed.
 const nav_toggle = document.querySelector(".nav-toggle");
 const nav_links = document.querySelectorAll(".nav-link");
 
-nav_toggle.addEventListener("click", function(){
+nav_toggle.addEventListener("click", function () {
     document.body.classList.toggle("nav-open");
 })
 
-nav_links.forEach(function(link){
-    link.addEventListener("click", function(){
+nav_links.forEach(function (link) {
+    link.addEventListener("click", function () {
         document.body.classList.remove("nav-open");
     })
 })
@@ -18,16 +17,16 @@ nav_links.forEach(function(link){
 const value = document.querySelector(".our-value");
 const valueBtns = document.querySelectorAll(".value-btn");
 const contents = document.querySelectorAll(".value-content");
-value.addEventListener("click", function(e){
+value.addEventListener("click", function (e) {
     const id = e.target.dataset.id;
-    if(id) {
-        valueBtns.forEach(function(btn){
+    if (id) {
+        valueBtns.forEach(function (btn) {
             btn.classList.remove("active");
         })
         e.target.classList.add("active");
 
 
-        contents.forEach(function(content){
+        contents.forEach(function (content) {
             content.classList.remove("active");
         })
         const element = document.getElementById(id);
@@ -76,6 +75,44 @@ const reviews = [
     },
 ];
 
+
+const name = document.querySelector(".review-name");
+const img = document.querySelector(".review-img");
+const job = document.querySelector(".review-job-title");
+const text = document.querySelector(".review-text");
+
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+let counter = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+    showPerson(counter);
+});
+
+nextBtn.addEventListener("click", function(){
+    counter++;
+    if(counter > reviews.length-1){
+        counter = 0;
+    }
+    showPerson(counter);
+})
+prevBtn.addEventListener("click", function (){
+    counter--;
+    if(counter < 0){
+        counter = reviews.length-1;
+    }
+    showPerson(counter);
+})
+
+function showPerson(index) {
+    const profile = reviews[index];
+    name.textContent = profile.name;
+    img.src = profile.img;
+    job.textContent = profile.job;
+    text.textContent = profile.text;
+
+}
 
 
 
