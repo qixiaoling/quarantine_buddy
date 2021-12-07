7cb8d7e35a2d5da4316c1adb30eb8de6f92e4c62
qixiaoling <qixiaolinglinda@hotmail.com>
2021-12-07 10:58:15 +0100

    app correction 5

--- questions.js
+++ questions.js
@@ -1,3 +1,22 @@
+
+//Navbar: by clicking the hamburger menu, one will add the classList to the body.
+//when body has the "nav-open" class, the hidden nav will translate back to 0 and show itself.
+//by clicking any of the link, the classList on the body will be removed.
+
+
+const nav_toggle = document.querySelector(".nav-toggle");
+const nav_links = document.querySelectorAll(".nav-link");
+
+nav_toggle.addEventListener("click", function () {
+    document.body.classList.toggle("nav-open");
+})
+
+nav_links.forEach(function (link) {
+    link.addEventListener("click", function () {
+        document.body.classList.remove("nav-open");
+    })
+})
+
 const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function(btn){
